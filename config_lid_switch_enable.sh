fa887da8fccabee678877ffd91657e64051ef35b
Rodolfo Santos <rodolfomoreira88@gmail.com>
2025-04-24 22:13:00 -0300

    Ajuste Enable

--- config_lid_switch_enable.sh
+++ config_lid_switch_enable.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# Verificar se o arquivo já contém as configurações necessárias
-FILE="/etc/systemd/logind.conf"
+sudo sed -i 's/^HandleLidSwitch=/#HandleLidSwitch=/' /etc/systemd/logind.conf
+sudo sed -i 's/^HandleLidSwitchDocked=/#HandleLidSwitchDocked=/' /etc/systemd/logind.conf
 
-if grep -q "^HandleLidSwitch=" "$FILE" && grep -q "^HandleLidSwitchDocked=" "$FILE"; then
-  echo "As configurações já estão presentes no arquivo."
-else
-  # Se não, adicionamos as configurações no final do arquivo
-  echo -e "\n#HandleLidSwitch=ignore\n#HandleLidSwitchDocked=ignore" | sudo tee -a "$FILE" > /dev/null
-  echo "As configurações foram adicionadas ao arquivo $FILE."
-fi
-
-# Reiniciar o serviço para aplicar as alterações
+# Reinicia o serviço pra aplicar
 sudo systemctl restart systemd-logind
-echo "Configurações aplicadas. O serviço foi reiniciado."
+echo "Suspensão reativada! As linhas foram comentadas."
