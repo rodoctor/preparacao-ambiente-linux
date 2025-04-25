06aa7cb2591fe7fbaa337a46a266be8ed941c372
Rodolfo Santos <rodolfomoreira88@gmail.com>
2025-04-24 22:01:33 -0300

    Ajustes

--- start.sh
+++ start.sh
@@ -4,15 +4,22 @@ sudo apt upgrade -y
 
 # Instalar o docker
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker
+sudo usermod -aG docker $USER
 
 # Instalar SSH Server
 sudo apt install -y openssh-server
 
 # Instalar OhMyZSH
-sudo apt install zsh
+sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/rodoctor/preparacao-ambiente-linux/refs/heads/main/theme_zsh.sh)"
 
 # Desativar suspensao de energia
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/rodoctor/preparacao-ambiente-linux/refs/heads/main/config_lid_switch.sh)"
+read -p "Deseja desativar suspensão de energia ao fechar a tampa? (s/N): " opcao
+if [ "$opcao" = "desativar" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/rodoctor/preparacao-ambiente-linux/refs/heads/main/config_lid_switch_disable.sh)"
+elif [ "$opcao" = "ativar" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/rodoctor/preparacao-ambiente-linux/refs/heads/main/config_lid_switch_enable.sh)"
+else
+  echo "Opção inválida: $opcao"
+fi
