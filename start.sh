dfc9aa3aa8f3daaaaffed9b3f2e81df86ce7ef05
Rodolfo Santos <rodolfomoreira88@gmail.com>
2025-04-24 22:37:45 -0300

    Ajuste start.sh

--- start.sh
+++ start.sh
@@ -9,11 +9,6 @@ sudo usermod -aG docker $USER
 # Instalar SSH Server
 sudo apt install -y openssh-server
 
-# Instalar OhMyZSH
-sudo apt install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/rodoctor/preparacao-ambiente-linux/refs/heads/main/theme_zsh.sh)"
-
 # Desativar suspensao de energia
 read -p "Deseja desativar suspensão de energia ao fechar a tampa? (s/N): " opcao
 opcao=$(echo "$opcao" | tr '[:upper:]' '[:lower:]')
@@ -25,3 +20,9 @@ elif [ "$opcao" = "n" ] || [ -z "$opcao" ]; then
 else
   echo "Opção inválida: $opcao"
 fi
+
+# Instalar OhMyZSH
+sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/rodoctor/preparacao-ambiente-linux/refs/heads/main/theme_zsh.sh)"
+zsh
